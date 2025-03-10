7d7f8c41b4d6c674eabfa041e5f8026475286547
vroobel95 <ada.wrobel95@gmail.com>
2025-03-10 15:08:39 +0100

    Updated eslint config, package-lock.json

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import pluginJs from '@eslint/js';
+
+import pluginReact from 'eslint-plugin-react';
+
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: 'import', next: '*' },
+        { blankLine: 'never', prev: 'import', next: 'import' },
+        { blankLine: 'always', prev: '*', next: 'return' },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
